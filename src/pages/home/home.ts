dbc7a8e8ac6e5e3257573b7161c384c254a1b593
nourZ12 <51907752+nourZ12@users.noreply.github.com>
2019-09-21 22:53:44 +0300

    Main Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SignupPage } from '../signup/signup';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,8 @@ export class HomePage {
 
   }
 
+  onSignUp() {
+    this.navCtrl.push(SignupPage);
+  }
+  
 }
